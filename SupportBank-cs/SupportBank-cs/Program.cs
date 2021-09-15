72613aafba40023fe7dd4f1dd822c317d3dfd47a
Olaf Davis <olaf.davis@softwire.com>
2021-09-15 12:25:37 +0100

    created blank c sharp project

---
+++ SupportBank-cs/SupportBank-cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SupportBank_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
